2e762b1ef13abf63c3ec37993d56b83a6ccab0c2
Quintus Cardozo <qcardozo@getlegaltech.com>
2025-06-01 18:45:44 +1000

    feat: added dock items

--- flake.nix
+++ flake.nix
@@ -88,6 +88,79 @@
 	      };
       };
 
+      system = {
+        defaults = {
+          dock = {
+            show-recents = false;
+            persistent-apps = [
+              {
+                app = "${pkgs.vscode.outPath}/Applications/Visual Studio Code.app";
+              }
+              {
+                app = "/Applications/Homebrew Apps/Ghostty.app";
+              }
+              {
+                app = "/Applications/Homebrew Apps/OrbStack.app";
+              }
+              {
+                spacer = {
+                  small = true;
+                };
+              }
+              {
+                app = "${pkgs.discord.outPath}/Applications/Discord.app";
+              }
+              {
+                app = "${pkgs.slack.outPath}/Applications/Slack.app";
+              }
+              {
+                app = "${pkgs.teams.outPath}/Applications/Teams.app";
+              }
+              {
+                app = "/Applications/Homebrew Apps/Wrike for Mac.app";
+              }
+              {
+                spacer = {
+                  small = true;
+                };
+              }
+              {
+                app = "${pkgs._1password-gui.outPath}/Applications/1Password.app";
+              }
+              {
+                app = "/Applications/Homebrew Apps/SafeInCloud Password Manager.app";
+              }
+              {
+                spacer = {
+                  small = true;
+                };
+              }
+              {
+                app = "/Applications/Homebrew Apps/Zen.app";
+              }
+              {
+                app = "/Applications/Safari.app";
+              }
+              {
+                spacer = {
+                  small = true;
+                };
+              }
+              {
+                app = "/Applications/Homebrew Apps/Yaak.app";
+              }
+              {
+                app = "/Applications/TeamViewer.app";
+              }
+            ];
+            persistent-others = [
+              "/Users/quintisimo"
+              "/Users/quintisimo/Downloads"
+            ];
+          };
+        };
+      };
+
       system.primaryUser = "quintisimo";
       users = {
 	      knownUsers = ["quintisimo"];
