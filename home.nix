65431a4f1c3e1cc143aadf84a6939356a56f18c9
Quintus Cardozo <quintuscardozo13@gmail.com>
2025-06-02 04:12:19 +1000

    Update vscode settings and add cron jobs

--- home.nix
+++ home.nix
@@ -133,18 +133,85 @@
             "vim.camelCaseMotion.enable" = true;
             "vim.highlightedyank.enable" = true;
             "vim.smartRelativeLine" = true;
-            "vim.statusBarColorControl" = true;
             "vim.useSystemClipboard" = true;
-            "workbench.colorCustomizations" = {
-              "statusBar.background" = "#5f0000";
-              "statusBar.noFolderBackground" = "#5f0000";
-              "statusBar.debuggingBackground" = "#5f0000";
-              "statusBar.foreground" = "#ffffff";
-              "statusBar.debuggingForeground" = "#ffffff";
-            };
           };
         };
       };
     };
+  };
+
+  
+  # Cron Jobs
+  launchd = {
+    enable = true;
+    agents = {} // (let 
+        dn = "dependabot-notifications";
+        pgr = "prune-git-repos";
+      in {
+        ${dn} = {
+          enable = true;
+          config = {
+            Program = "${pkgs.writeShellApplication {
+              name = dn;
+              runtimeInputs = [
+                pkgs.gh
+                pkgs.terminal-notifier
+              ];
+              text = ''
+                orgs=$(gh org list)
+                open_pr_repos=""
+
+                for org in $orgs; do
+                  repos=$(gh repo list "$org" --json nameWithOwner -q '.[].nameWithOwner')
+
+                  for repo in $repos; do
+                    repo_pr_count=$(gh pr list --author app/dependabot --state open --repo "$repo" --json url -q '.[].url' | wc -l)
+
+                    if [ "$repo_pr_count" -gt 0 ]; then
+                      open_pr_repos+="$repo"$'\n'
+                    fi
+                  done
+                done
+
+                for open_pr_repo in $open_pr_repos; do
+                  url="https://github.com/$open_pr_repo/pulls"
+                  terminal-notifier -title "Open dependabot prs" -message "$open_pr_repo" -open "$url"
+                done
+              '';
+            }}/bin/${dn}";
+            StartCalendarInterval = [
+              {
+                Minute = 0;
+                Hour = 9;
+                Day = 2;
+              }
+            ];
+        };
+      };
+      ${pgr} = {
+        enable = true;
+        config = {
+          Program = "${pkgs.writeShellApplication {
+            name = pgr;
+            runtimeInputs = [
+              pkgs.gh-poi
+            ];
+            text = ''
+              find ~/Github -type d -name ".git" | while read -r dir; do
+                repo_dir=$(dirname "$dir")
+                cd "$repo_dir" && gh-poi
+              done
+            '';
+          }}/bin/${pgr}";
+          StartCalendarInterval = [
+            {
+              Minute = 0;
+              Hour = 9;
+              Day = 2;
+            }
+          ];
+        };
+      };
+    });
   };
 }
