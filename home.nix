89548894d8abf9b8223161b25e4fc1cabf43f95e
Quintus Cardozo <quintuscardozo13@gmail.com>
2025-06-04 10:15:14 +1000

    Enable key repeat for vscode

--- home.nix
+++ home.nix
@@ -15,6 +15,16 @@
     };
   };
 
+  targets = {
+    darwin = {
+      defaults = {
+        "com.microsoft.VSCode" = {
+          ApplePressAndHoldEnabled = false;
+        };
+      };
+    };
+  };
+
   programs = {
     home-manager = {
       enable = true;
