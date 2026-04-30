c67caa5274471b9dcb14469c2274abd917ae5659
Quintus Cardozo <quintuscardozo13@gmail.com>
2026-04-30 16:51:50 +1000

    feat: add shortcut to launch and kill work apps

--- home/services/skhd.nix
+++ home/services/skhd.nix
@@ -6,12 +6,26 @@
 }:
 {
   config = {
-    services.skhd = {
-      enable = true;
-      config = ''
-        fn - c : ${config.zedCli} ${osConfig.folders.nix}
-      '';
-    };
+    services.skhd =
+      let
+        workAppsAction =
+          action:
+          builtins.concatStringsSep " && " (
+            map (app: "${action} \"${app}\"") [
+              "Slack"
+              "Discord"
+              "Mail"
+            ]
+          );
+      in
+      {
+        enable = true;
+        config = ''
+          fn - c : ${config.zedCli} ${osConfig.folders.nix}
+          fn - w : ${workAppsAction "open -a"}
+          fn + shift - w : ${workAppsAction "killall"}
+        '';
+      };
     home.activation.reloadSkhdConfig = ''
       echo "Reloading skhd config..."
       ${pkgs.skhd}/bin/skhd -r
